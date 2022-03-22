7b9c2abd6608f5cced1caf5a07815fe75f92b0f8
Sharne7 <101091214+Sharne7@users.noreply.github.com>
2022-03-22 15:15:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "devopsaa.database.windows.net";
-    $user = "devops";
-    $pass = "Thape3487#";
-    $db = "devopsaa";
+    $host = "azureuser123.database.windows.net";
+    $user = "anderson";
+    $pass = "Asharne12345";
+    $db = "azureuser123";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
@@ -45,7 +45,7 @@
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO gg (name, email, job, date) 
+            $sql_insert = "INSERT INTO lists (name, email, job, date) 
                         VALUES ('$name', '$email', '$job', '$date')";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
